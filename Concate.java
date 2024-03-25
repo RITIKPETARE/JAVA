58d92d308b6c8e31263f5f6da48d45131e6dad42
RITIKPETARE <138520553+RITIKPETARE@users.noreply.github.com>
2024-03-26 00:44:41 +0530

    Add files via upload

---
+++ Concate.java
@@ -0,0 +1,13 @@
+class Concate{
+	public static void main(String[]args){
+	
+	int a = 10 , b = 20  ;
+	System.out.println("Value of a ="+a);
+	System.out.println("Value of b ="+b);
+
+	System.out.println("sum ="+a+b);
+	System.out.println( a + b + "hello world");	
+	System.out.println("hello" +(a+b)+ "hello");
+
+}
+}
