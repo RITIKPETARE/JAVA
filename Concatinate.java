b2f62aba39ef5b6a0122b2e091b90f8fb473a61f
RITIKPETARE <138520553+RITIKPETARE@users.noreply.github.com>
2024-03-26 00:27:25 +0530

    Add files via upload

---
+++ Concatinate.java
@@ -0,0 +1,6 @@
+class Concate{
+	public static void main(String[]args){
+	
+	int a = 10 , b = 20;
+	System.out.println("Value of a ="+a);
+	System.out.println("Value of b ="+b);
---
+++ Sum.java
@@ -0,0 +1,16 @@
+ import java.util.*;
+class Sum {
+
+  public static void main(String[] args){
+
+	System.out.println("Enter the value of a and b :");
+	Scanner sc = new Scanner(System.in);
+	
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int sum = a + b;
+	System.out.println(sum);
+	
+	
+}
+}
