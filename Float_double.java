e901ac065af5ec6902eafa9191c077c7e15ef6e1
RITIKPETARE <138520553+RITIKPETARE@users.noreply.github.com>
2024-03-27 01:52:52 +0530

    Add files via upload

---
+++ Float_double.java
@@ -0,0 +1,22 @@
+class Float_double{
+	public static void main(String[]args){
+	
+	float a = 10.53f ;		// f is use to show it is a float value
+
+	double b = 20.42 ; 		// it is double value
+ 	System.out.println(a);
+	System.out.println(b);
+
+	char ch1 = 'A';   		// normal char
+	char ch2 = 65;	  		// ASCII code = A
+	char ch3 = '\u0041';    // java typecode
+
+	System.out.println(ch1);
+	System.out.println(ch2);
+	System.out.println(ch3);
+
+
+}
+
+
+}
